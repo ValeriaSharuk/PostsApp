5ecded607ee21da09f665c75ee7e688fc54a6637
Lera Sharuk <lera.sharuk@mail.ru>
2022-09-06 17:07:42 +0300

    added fetching with API, navigation, pagination

--- src/App.js
+++ src/App.js
@@ -1,59 +1,17 @@
 import './style/App.css';
-import {useMemo, useState} from "react";
-import PostForm from "./components/PostForm";
-import PostList from "./components/PostList";
-import MySelect from "./components/UI/select/MySelect";
-import MyInput from "./components/UI/input/MyInput";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import MyButton from "./components/UI/button/MyButton";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
 
 function App() {
-  const [posts, setPosts] = useState([
-      {id:1, title: "Javascript", body: "1"},
-      {id:2, title: "Kotlin", body: "2"},
-      {id:3, title: "C", body: "3"},
-  ]);
-  const [filter, setFilter] = useState({sort: '', query:''});
-  const [modal, setModal] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-      console.log('Отр')
-      if(filter.sort){
-          return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-      }
-      return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo( () => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query,sortedPosts])
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-      setModal(false)
-  }
-
-  const removePost = (post) => {
-      setPosts(posts.filter(p => p.id !== post.id))
-  }
-
-    return (
-        <div className="App">
-            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-                Создать пользователя
-            </MyButton>
-            <MyModal visible={modal} setVisible={setModal}>
-                <PostForm create={createPost}/>
-            </MyModal>
-            <hr style={{margin: '15px 0'}}/>
-            <PostFilter
-                filter={filter}
-                setFilter={setFilter}
-            />
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про языки программирования"/>
-        </div>
-  );
+  return(
+      <BrowserRouter>
+          <Routes>
+              <Route path="/about" element={<About/>}/>
+              <Route path="/posts" element={<Posts/>}/>
+          </Routes>
+      </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Лучшее приложение для создания и чтения постов!
+        </h1>
+    );
+};
+
+export default About;
